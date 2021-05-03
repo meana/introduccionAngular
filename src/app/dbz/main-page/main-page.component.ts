af075a7165dbb3b718a8f017e6c50834212f55ac
meana <ramon.meana@gmail.com>
2021-05-03 19:47:32 +0200

    @input

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-interface Personaje{
-  nombre: string;
-  poder: number
-}
+import { Personaje } from '../interfaces/dbz.interfaces';
 
 @Component({
   selector: 'app-main-page',
--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interfaces';
 
 @Component({
   selector: 'app-personajes',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class PersonajesComponent {
 
-  personajes: any[] = [];
+  @Input()
+  personajes: Personaje[] = [];
 }
