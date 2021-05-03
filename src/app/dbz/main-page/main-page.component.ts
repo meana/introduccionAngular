56012dd6b2664a8133bdb84682378d3c62e6c7ef
meana <ramon.meana@gmail.com>
2021-05-03 19:30:45 +0200

    58 Module

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -12,12 +12,30 @@ interface Personaje{
 })
 export class MainPageComponent {
 
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta', 
+      poder: 7500
+    }
+  ]
+
   nuevo: Personaje = {
-    nombre: 'Trunks',
-    poder: 14000
+    nombre: '',
+    poder: 0
   }
 
   agregar(){
-    console.log(this.nuevo);
+    if(this.nuevo.nombre.trim().length === 0){
+      return;
+    }
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
   }
 }
