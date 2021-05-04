180102e57c7eb1e4e76bc3ea10f991c2b85350d3
meana <ramon.meana@gmail.com>
2021-05-04 19:41:03 +0200

    Centralize data in service

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interfaces';
-import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
@@ -9,27 +8,10 @@ import { DbzService } from '../services/dbz.service';
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta', 
-      poder: 7500
-    }
-  ]
-
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
     poder: 1000
   }
 
-  agregarNuevoPersonaje(arg: Personaje){
-    this.personajes.push(arg);
-  }
-
-  constructor(private dbzService: DbzService) {
-
-  }
+  constructor() { }
 }
